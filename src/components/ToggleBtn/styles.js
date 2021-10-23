35c42826c634061bb4e24595656844fb038d059f
Bernardo Bevilaqua <bernardo.bfg@hotmail.com>
2021-10-23 18:48:28 -0300

    melhora estilizacao do botao

--- src/components/ToggleBtn/styles.js
+++ src/components/ToggleBtn/styles.js
@@ -1,10 +1,10 @@
 import styled from 'styled-components';
 
-const defineGradient = (colour, themeName) => {
+const defineGradient = (themeName) => {
   if (themeName === 'light') {
-    return "90deg, rgba(5,5,55,1) 0%, rgba(2,0,36,1) 15%, rgba(0,212,255,1) 100%"
+    return "90deg, rgba(5,5,55,1) 0%, rgba(2,0,36,1) 0%, rgba(0,212,255,1) 100%"
   }
-  return "90deg, rgba(115,64,187,0.9612219887955182) 44%, rgba(85,60,128,1) 92%, rgba(89,51,136,1) 100%"
+  return "90deg, rgba(115,64,187,0.9612219887955182) 44%, rgba(85,60,128,1) 80%, rgba(70,40,120,1) 100%"
 }
 
 export const ToggleDiv = styled.div`
@@ -26,7 +26,7 @@ export const ToggleDiv = styled.div`
       justify-content: center;
       z-index: 10;
       align-items:center;
-      background: linear-gradient(${props => defineGradient(props.theme.primary, props.themeName)} );
+      background: linear-gradient(${props => defineGradient(props.themeName)} );
       img{
         width: 18px;
         height: 18px;
